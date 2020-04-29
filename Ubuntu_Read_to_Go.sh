37f0b1ff3d73b6d6dfa8417cfc81e5de1e64d9b1
Guilherme Aquino Lima Barroso <guiaqui23@gmail.com>
2020-04-29 01:55:10 -0300

    Update Ubuntu_Read_to_Go.sh

--- Ubuntu_Read_to_Go.sh
+++ Ubuntu_Read_to_Go.sh
@@ -8,7 +8,6 @@ sudo chown root:root bxt_guc_ver8_7.bin kbl_guc_ver9_14.bin
 sudo mv bxt_guc_ver8_7.bin kbl_guc_ver9_14.bin /lib/firmware/i915
 sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
 #Download Git for Linux and Unix
-sudo apt purge git -y
 sudo add-apt-repository ppa:git-core/ppa -y
 sudo apt update
 sudo apt install git -y
@@ -17,17 +16,23 @@ sudo apt install curl
 curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt update
-sudo apt install spotify-client
+sudo apt install spotify-client -y
 #Chromium stable
 sudo add-apt-repository ppa:chromium-team/stable -y
 sudo apt update
 sudo apt install chromium-browser -y
+#Only in GnomeUbuntu sudo apt-get install chrome-gnome-shell -y
 #qBittorrent Stable
-sudo add-apt-repository ppa:qbittorrent-team/qbittorrent-stable
+sudo add-apt-repository ppa:qbittorrent-team/qbittorrent-stable -y
 sudo apt update
-sudo apt install qbittorrent
+sudo apt install qbittorrent -y
 #Firefox ESR
-sudo apt purge firefox firefox-locale-en firefox-locale-pt
-sudo add-apt-repository ppa:mozillateam/ppa
+sudo apt purge firefox firefox-locale-en firefox-locale-pt unity-scope-firefoxbookmarks -y
+sudo add-apt-repository ppa:mozillateam/ppa -y
 sudo apt update
 sudo apt install firefox-esr firefox-esr-locale-pt
+#Installing LibreOffice on Linux
+sudo apt purge libreoffice-common
+sudo add-apt-repository ppa:libreoffice/ppa -y
+sudo apt update
+sudo apt install libreoffice-common libreoffice-help-pt-br libreoffice-l10n-pt-br -y
