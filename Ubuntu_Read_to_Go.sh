327055565099df5aa2cadbc1ff12456ad9399007
Guilherme Aquino Lima Barroso <guiaqui23@gmail.com>
2021-04-23 18:36:32 -0300

    Update Ubuntu_Read_to_Go.sh

--- Ubuntu_Read_to_Go.sh
+++ Ubuntu_Read_to_Go.sh
@@ -2,6 +2,7 @@
 sudo apt install wget		#Package downloader
 sudo apt install gdebi		#Debian package installer
 sudo apt install ppa-purge	#PPA remover
+sudo apt install default-jdk	#Install Java Development Kit
 case codename=$(lsb_release --codename --short) in
 xenial)
 	#Fixing "W: Possible missing firmware /lib/firmware/i915/kbl_guc_ver9_14.bin for module i915"
